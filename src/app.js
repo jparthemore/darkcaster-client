336dbb7c1d3a6ae753f972f86d58c4da1ce94e48
Jan Parthemore <janparthemore@gmail.com>
2017-07-05 20:57:27 -0400

    our first angular app

--- src/app.js
+++ src/app.js
@@ -1,2 +1,16 @@
 // require('./app.css');
 // console.log('It works');
+const angular = require('angular');
+
+//console.log(angular);
+
+//create our applicaton
+const app = angular.module('darkcaster-client',[]);//setter syntax
+
+app.controller('MainController', MainController);
+MainController.$inject = ['$scope']; //magic for us
+
+function MainController($scope){
+  $scope.message = 'hello from angular';
+  const crowbar = 'hellow from angular again';
+}
