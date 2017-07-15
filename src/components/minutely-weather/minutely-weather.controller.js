6183fb43fd83978ab62e069aa7c2c8f49f51f40b
Jan Parthemore <janparthemore@gmail.com>
2017-07-15 19:30:15 -0400

    html display done for minutely weather

--- src/components/minutely-weather/minutely-weather.controller.js
+++ src/components/minutely-weather/minutely-weather.controller.js
@@ -2,12 +2,14 @@
 
 //const weatherData = require('../../../mocks/weather.json');
 
-MinutelyWeatherController.$inject=['WeatherService'];
+MinutelyWeatherController.$inject=['WeatherService','ImageService'];
 
-function MinutelyWeatherController (weather){
+function MinutelyWeatherController (weather,images){
   //this.weatherMinutely = weatherData.minutely;
   this.lat = 32;
   this.lon = -127;
+  this.imageLookup = images.imageLookup;
+  this.missingImage = images.missingImage;
   weather.getMinutelyWeather(this.lat,this.lon)
                                 .then(resp=>this.weatherMinutely=resp);
 }
