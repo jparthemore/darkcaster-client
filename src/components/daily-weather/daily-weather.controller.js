1daa1ebd7a86e07f318e1ba5fa72a18ea8118ef2
Jan Parthemore <janparthemore@gmail.com>
2017-07-16 14:41:35 -0400

    set up daily component to use weather service for daily weather

--- src/components/daily-weather/daily-weather.controller.js
+++ src/components/daily-weather/daily-weather.controller.js
@@ -1,11 +1,16 @@
 /*jshint esversion: 6*/
 
-const weatherData = require('../../../mocks/weather.json');
+//const weatherData = require('../../../mocks/weather.json');
 
-DailyWeatherController.$inject = [];
+DailyWeatherController.$inject = ['WeatherService'];
 
-function DailyWeatherController(){
-  this.weatherDaily = weatherData.daily;
+function DailyWeatherController(weather){
+  //this.weatherDaily = weatherData.daily;
+  this.lat = 29;
+  this.lon = -82;
+
+  weather.getDailyWeather(this.lat,this.lon)
+         .then(resp=>this.weatherDaily=resp);
 
 }
 module.exports = DailyWeatherController;
